2d79fa4ca85c45e23c94586d430f88c13820b62d
a.teder <alexteder@Alexs-MacBook-Air.local>
2026-07-27 21:42:08 +0400

    change indent line color

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,6 +2,12 @@ vim.pack.add({
   "https://github.com/lukas-reineke/indent-blankline.nvim",
 })
 
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function()
+    vim.api.nvim_set_hl(0, "IblIndent", { link = "NonText" })
+  end,
+})
+
 require("ibl").setup({
   indent = { char = "┊" },
   scope = { enabled = false },
